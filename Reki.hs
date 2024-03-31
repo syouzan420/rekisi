42777516658504d677995d4fbb650d2b8eeb7826
syouzan420 <syouzanponkikki@yahoo.co.jp>
2024-03-31 11:22:26 +0900

    update

--- Reki.hs
+++ Reki.hs
@@ -4,12 +4,13 @@ module Reki where
 import qualified Data.Text as T
 import File (fileRead)
 
-data KWord = Hito T.Text | Mono T.Text | Koto T.Text deriving (Eq,Show)
+data KWord = Hito T.Text | Mono T.Text | Atum T.Text | Koto T.Text deriving (Eq,Show)
 
 type Nen = Int
 type Disc = T.Text
+data Grade = A | B | C deriving (Eq,Show)
 
-data HData = HData Nen KWord Disc [KWord] deriving (Eq,Show)
+data HData = HData Grade Nen Disc [KWord] deriving (Eq,Show)
 
 rekifile :: FilePath
 rekifile = "reki.txt"
@@ -26,17 +27,25 @@ makeHData :: T.Text -> HData
 makeHData txt =
   let sps = T.splitOn "," txt
       pdt = if length sps > 3 then sps else fillList sps
-      (n:k:d:ks) = pdt
-      nen = read$T.unpack n
-      kw = makeKW k
+      (n:d:ks) = pdt
+      (gr,nen) = makeNen n 
       kws = map makeKW ks
-   in HData nen kw d kws
+   in HData gr nen d kws
+
+makeNen :: T.Text -> (Grade,Nen)   
+makeNen txt
+  | txt==T.empty = (C,0)
+  | T.head txt=='!' = (B,(read . T.unpack . T.tail) txt)
+  | otherwise = (A,(read. T.unpack) txt)
 
 makeKW :: T.Text -> KWord
-makeKW k = case T.head k of
-             'h' -> Hito (T.tail k)
-             'm' -> Mono (T.tail k)
-             'k' -> Koto (T.tail k)
+makeKW k = 
+  if k==T.empty then Koto " " else let tk = T.tail k in
+          case T.head k of
+             'h' -> Hito tk 
+             'm' -> Mono tk 
+             'k' -> Koto tk 
+             'a' -> Atum tk 
              _   -> Koto k
 
 fillList :: [T.Text] -> [T.Text]
